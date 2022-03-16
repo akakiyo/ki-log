7d4a8e3c7428a9f4dc09750e0d5a592049036eb9
kiyo <kiyo.a328@gmail.com>
2022-03-16 22:02:50 +0900

    ファイル名の追加

--- components/MarkDownStyle/CodeBlock.js
+++ components/MarkDownStyle/CodeBlock.js
@@ -1,17 +1,36 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dark } from "react-syntax-highlighter/dist/cjs/styles/prism";
-
+import styled from "styled-components";
 const CodeBlock = ({ inline, className, children, ...props }) => {
   const match = /language-(\w+)/.exec(className || "");
+  // console.log("match", match);
+  // console.log("className", className);
+  // console.log("children", children);
+  // console.log("props", props);
+  const fileName = match?.["input"].split(":")[1];
+  console.log(fileName);
+
   return !inline && match ? (
-    <SyntaxHighlighter style={dark} language={match[1]}>
-      {String(children).replace(/\n$/, "")}
-    </SyntaxHighlighter>
+    <>
+      <FileNameWrapper>&nbsp;{fileName}</FileNameWrapper>
+      <SyntaxHighlighter style={dark} language={match[1]}>
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    </>
   ) : (
     <code className={className} {...props}>
       {children}
     </code>
   );
 };
+
+const FileNameWrapper = styled.p`
+  position: absolute;
+  width: fit-content;
+  margin: 0 0 0 0.5vw;
+  color: #ffffff;
+  border-bottom: #ffffff 1px solid;
+  z-index: 100;
+`;
 
 export default CodeBlock;
